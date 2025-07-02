6ad80ace52f38106bf3d575e6d9db49909e557e0
Sarawut <sarawootchaiwong3@gmail.com>
2025-07-02 11:49:52 +0700

    card deploy commit

--- src/app/component/card.js
+++ src/app/component/card.js
@@ -13,7 +13,7 @@ export default function Card() {
     
 <div className="col-md-2 mb-4">
 <div className="card shadow-sm">    
-  <img className="rounded img-responsive" src="/images/roblox.png" alt="..." />
+  <Image className="rounded img-responsive" src="/images/roblox.png" alt="..." />
   <div className="card-body">
     <h5 className="card-title text-primary">Home</h5>
     <a href="#" className="btn btn-primary btn-sm">Learn More</a>
@@ -23,7 +23,7 @@ export default function Card() {
 
 <div className="col-md-2 mb-4">
 <div className="card shadow-sm">    
-  <img className="rounded img-responsive" src="/images/roblox2.png" alt="..." />
+  <Image className="rounded img-responsive" src="/images/roblox2.png" alt="..." />
   <div className="card-body">
     <h5 className="card-title text-success">Service</h5>
     <a href="#" className="btn btn-success btn-sm">Learn More</a>
@@ -33,7 +33,7 @@ export default function Card() {
 
 <div className="col-md-2 mb-4">
 <div className="card shadow-sm">    
-  <img className="rounded img-responsive" src="/images/roblox3.png" alt="..." />
+  <Image className="rounded img-responsive" src="/images/roblox3.png" alt="..." />
   <div className="card-body">
     <h5 className="card-title text-warning">About</h5>
     <a href="#" className="btn btn-warning btn-sm text-white">Learn More</a>
@@ -43,7 +43,7 @@ export default function Card() {
 
 <div className="col-md-2 mb-4">
 <div className="card shadow-sm">    
-  <img className="rounded img-responsive" src="/images/roblox4.png" alt="..." />
+  <Image className="rounded img-responsive" src="/images/roblox4.png" alt="..." />
   <div className="card-body">
     <h5 className="card-title text-danger">Service</h5>
     <a href="#" className="btn btn-danger btn-sm">Learn More</a>
